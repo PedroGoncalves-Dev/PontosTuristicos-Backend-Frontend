8ba23157d011a5e0ac301566cfc38204fb7b0aad
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2025-01-12 14:46:20 -0300

    rotas e layout da aplicação criado

--- Frontend-PontoTuristico/src/App.tsx
+++ Frontend-PontoTuristico/src/App.tsx
@@ -1,7 +1,33 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 
+import "./App.css";
+import NavBar from "./Components/navBar";
+import Footer from "./Components/footer";
+import Home from "./Pages/Home";
+
+const MainLayout = () => {
+  return (
+    <>
+      <NavBar />
+      <main className="min-h-[calc(100vh-3rem)]">
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+};
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
