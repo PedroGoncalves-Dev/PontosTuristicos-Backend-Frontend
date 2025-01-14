4c7603504021ac68fbcb4786418429ec9b2e78e9
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2025-01-14 01:16:15 -0300

    roda pe pronto

--- Frontend-PontoTuristico/src/Components/footer/index.tsx
+++ Frontend-PontoTuristico/src/Components/footer/index.tsx
@@ -1,7 +1,23 @@
+import { Github, Linkedin } from "lucide-react";
+
 const Footer = () => {
   return (
-    <footer className="bg-orange-900 h-24">
-      <h1>Roda pe</h1>
+    <footer className="flex flex-col items-center justify-center gap-5 bg-orange-900 h-36">
+      <div className="flex gap-2 ">
+        <a href="https://github.com/PedroGoncalves-Dev">
+          <div className="w-10 p-2 rounded-full bg-[#fafafa5d] hover:scale-105 transition-transform duration-200 ease-in-out">
+            <Github color="#000" />
+          </div>
+        </a>
+
+        <a href="https://www.linkedin.com/in/pedrodev-goncalves">
+          <div className="w-10 p-2 rounded-full bg-[#fafafa5d] hover:scale-105 transition-transform duration-200 ease-in-out">
+            <Linkedin color="#000" />
+          </div>
+        </a>
+      </div>
+
+      <p>&copy; 2025 - Pedro Henrique Gonçalves</p>
     </footer>
   );
 };
