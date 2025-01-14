8a227fcc28daefe0821411e14325eae56ccc9244
Pedro Henrique Gonçalves <pedro_developer@outlook.com>
2025-01-14 01:39:47 -0300

    Componente footer criado icones de redes sociais

--- Frontend-PontoTuristico/src/Components/footer/index.tsx
+++ Frontend-PontoTuristico/src/Components/footer/index.tsx
@@ -4,20 +4,30 @@ const Footer = () => {
   return (
     <footer className="flex flex-col items-center justify-center gap-5 bg-orange-900 h-36">
       <div className="flex gap-2 ">
-        <a href="https://github.com/PedroGoncalves-Dev">
+        <a
+          href="https://github.com/PedroGoncalves-Dev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <div className="w-10 p-2 rounded-full bg-[#fafafa5d] hover:scale-105 transition-transform duration-200 ease-in-out">
             <Github color="#000" />
           </div>
         </a>
 
-        <a href="https://www.linkedin.com/in/pedrodev-goncalves">
+        <a
+          href="https://www.linkedin.com/in/pedrodev-goncalves"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <div className="w-10 p-2 rounded-full bg-[#fafafa5d] hover:scale-105 transition-transform duration-200 ease-in-out">
             <Linkedin color="#000" />
           </div>
         </a>
       </div>
 
-      <p>&copy; 2025 - Pedro Henrique Gonçalves</p>
+      <p className="text-base text-gray-400">
+        &copy; 2025 - Pedro Henrique Gonçalves
+      </p>
     </footer>
   );
 };
